bb07ae1fd8e0cb0d6f0f1d3afd7b235c6a9d54da
shyamaprem <30853111+shyamaprem@users.noreply.github.com>
2017-09-06 13:46:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send('Article - one requested')
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send('Article - two requested')
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send('Article - three requested')
 });
 
 app.get('/ui/style.css', function (req, res) {
