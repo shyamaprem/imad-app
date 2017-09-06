73d2d62270130271bff202a77ce0b8bd36dbe0d8
shyamaprem <30853111+shyamaprem@users.noreply.github.com>
2017-09-06 14:35:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function createTemplate(data){
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(createTemplate(articleOne)
 });
 
 app.get('/article-one', function (req, res) {
