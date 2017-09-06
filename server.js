f0175d308791246f1cd7c9263bc1f6cee437cefb
shyamaprem <30853111+shyamaprem@users.noreply.github.com>
2017-09-06 14:33:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article-One',
+    heading:'Article-One',
+    date:'Sep 6 2017',
+    content:`   <p>
+                    This is my first time working with Node.js and I ran into this problem:
+                </p>
+                <p>
+                    I have started a Node server through the plugin of an IDE. Unfortunately, I cannot use the IDE's terminal. So I tried to run the script from the command line.
+                    
+                </p>
+                <p>
+                    This is the problem - I am using the Express module and my app is listening some port (8080). When I start the app from the command line, it throws this error:
+                    
+                </p>`
+};
+function createTemplate(data){
+    
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `<html>
+        <head>
+            <title> 
+              ${title}
+            </title>
+            <link href="/ui/style.css" rel="stylesheet">
+        </head>
+        
+        <body>
+            <div class="container">
+                <div>
+                    <a href = "/"> Home </a>
+                </div>
+                <div>
+                    <h3> ${heading}</h3>
+                </div>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
