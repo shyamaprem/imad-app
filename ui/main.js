f082eea909520c1b85cef3798f4be18fd30fcf21
shyamaprem <30853111+shyamaprem@users.noreply.github.com>
2017-09-07 10:25:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 console.log(element);
 element.innerHTML = 'New value';
 var img = document.getElementById('main-img');
-img.onClick = function(){
-  img.style.marginLeft = "10px"  ;
+img.onclick = function(){
+  img.style.marginLeft = "100px"  ;
 };
